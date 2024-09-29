bcc55555ae574233c51f6ae29862765a3b836f64
velteyn <velteyn@gmail.com>
2024-09-29 17:29:58 +0200

    Local file sync

--- scenes/SlotTile.gd
+++ scenes/SlotTile.gd
@@ -2,32 +2,36 @@ extends Node2D
 class_name SlotTile
 
 var size :Vector2
-
-func _ready():
-  pass
+var tween : Tween
+var speed : int
+	
+func _init():
+	pass
 
 func set_texture(tex):
-  $Sprite.texture = tex
-  set_size(size)
+	$Sprite2D.texture = tex
+	set_size(size)
 
 func set_size(new_size: Vector2):
-  size = new_size
-  $Sprite.scale = size / $Sprite.texture.get_size()
+	size = new_size
+	$Sprite2D.scale = size / $Sprite2D.texture.get_size()
   
-func set_speed(speed):
-  $Tween.playback_speed = speed
+func set_velocity(spd):
+	speed = spd
+	
   
 func move_to(to: Vector2):
-  $Tween.interpolate_property(self, "position",
-    position, to, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-  $Tween.start()
+	tween = get_tree().create_tween()
+	tween.set_speed_scale(speed)
+	tween.tween_property(self, "position",to , 1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT) 
+	tween.connect("finished",Callable(get_node("../../SlotMachine"), "_on_tile_moved").bind(self))  
 
 func move_by(by: Vector2):
-  move_to(position + by)
+	move_to(position + by)
   
 func spin_up():
-  $Animations.play('SPIN_UP')
+	$Animations.play('SPIN_UP')
   
 func spin_down():
-  $Animations.play('SPIN_DOWN')
+	$Animations.play('SPIN_DOWN')
   
